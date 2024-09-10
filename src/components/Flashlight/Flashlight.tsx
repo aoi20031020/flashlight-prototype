dbab625f69ae6aa158596798e79c276ef8e82986
aoi20031020 <aoi20031020@gmail.com>
2024-09-10 23:34:09 +0900

    クリック処理

--- src/components/Flashlight/Flashlight.tsx
+++ src/components/Flashlight/Flashlight.tsx
@@ -3,6 +3,7 @@ import "./Flashlight.css";
 
 const Flashlight: React.FC = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [isMouseDown, setIsMouseDown] = useState(false); // マウスがクリックされているかどうかを保持
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
@@ -12,16 +13,28 @@ const Flashlight: React.FC = () => {
       });
     };
 
+    const handleMouseDown = () => {
+      setIsMouseDown(true);
+    };
+
+    const handleMouseUp = () => {
+      setIsMouseDown(false);
+    };
+
     window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mousedown", handleMouseDown);
+    window.addEventListener("mouseup", handleMouseUp);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mousedown", handleMouseDown);
+      window.removeEventListener("mouseup", handleMouseUp);
     };
   }, []);
 
   return (
     <div
-      className="flashlight"
+      className={`flashlight ${isMouseDown ? "flashlight-active" : ""}`}
       style={{
         left: mousePosition.x - 100, // 光の中心をカーソルに合わせる
         top: mousePosition.y - 100,
